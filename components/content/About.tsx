92067987c6330e2c7f66794a8164fd171867fc25
elmars-hub <martinsifeanyi247@gmail.com>
2025-04-13 16:03:43 -0700

    fix: improve text flow and remove unnecessary line breaks in About component

--- components/content/About.tsx
+++ components/content/About.tsx
@@ -13,21 +13,17 @@ const About = () => {
         Hi, I&apos;m {personalInfo.name} a {personalInfo.role} passionate about
         crafting clean, responsive, and user-focused web experiences. My journey
         into tech started with a simple curiosity and grew into a love for
-        building everything from passion projects to real-world applications.
-        <br />
-        I specialize in React, Next.js, TypeScript, and Tailwind CSS, turning
+        building everything from passion projects to real-world applications. I
+        specialize in React, Next.js, TypeScript, and Tailwind CSS, turning
         designs into intuitive, pixel-perfect interfaces. My development
         approach is rooted in clean code, performance, accessibility, and
-        constant learning.
-        <br />
-        I believe in building in public, contributing to the dev community, and
-        learning through collaboration. If you&apos;re into frontend, anime, or
-        just love creating awesome stuff on the web let&apos;s connect and build
-        something great together!
-        <br />
+        constant learning. I believe in building in public, contributing to the
+        dev community, and learning through collaboration. If you&apos;re into
+        frontend, anime, or just love creating awesome stuff on the web
+        let&apos;s connect and build something great together!
       </p>
 
-      <p className="text-base sm:text-base md:text-lg text-foreground dark:text-white mt-4 text-justify">
+      <p className="text-base sm:text-base md:text-lg text-foreground dark:text-white mt-2 text-justify">
         If you&apos;re interested in collaborating on something exciting or need
         help with a project, feel free to reach out. Let&apos;s create something
         amazing together!
