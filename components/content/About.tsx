b925c24e70812fbe5278f930e12ff2a4c19cd513
elmars-hub <martinsifeanyi247@gmail.com>
2025-04-13 15:52:29 -0700

    fix: update text formatting for consistency in About component

--- components/content/About.tsx
+++ components/content/About.tsx
@@ -13,8 +13,8 @@ const About = () => {
         content={`Here is a little bit about me and my journey as a developer.`}
       />
 
-      <p className="text-base sm:text-base md:text-lg text-foreground dark:text-white">
-        Hi, I’m {personalInfo.name} a {personalInfo.role} passionate about
+      <p className="text-base sm:text-base md:text-lg text-foreground dark:text-white text-justify">
+        Hi, I&apos;m {personalInfo.name} a {personalInfo.role} passionate about
         crafting clean, responsive, and user-focused web experiences. My journey
         into tech started with a simple curiosity and grew into a love for
         building everything from passion projects to real-world applications.
@@ -26,15 +26,15 @@ const About = () => {
         <br />
         I believe in building in public, contributing to the dev community, and
         learning through collaboration. If you&apos;re into frontend, anime, or
-        just love creating awesome stuff on the web let’s connect and build
+        just love creating awesome stuff on the web let&apos;s connect and build
         something great together!
         <br />
       </p>
 
-      <p className="text-base sm:text-base md:text-lg text-foreground dark:text-white mt-4">
-        If you’re interested in collaborating on something exciting or need help
-        with a project, feel free to reach out. Let’s create something amazing
-        together!
+      <p className="text-base sm:text-base md:text-lg text-foreground dark:text-white mt-4 text-justify">
+        If you&apos;re interested in collaborating on something exciting or need
+        help with a project, feel free to reach out. Let&apos;s create something
+        amazing together!
       </p>
     </AnimationContainer>
   );
